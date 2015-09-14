0e99b73a20e62cd86deea5029b5464747352d25a
rgvwebpro <contact@rgvwebpro.com>
2015-09-14 14:09:45 -0500

    Yet, even more code cleanup
    It’s a mess, cleaning it all up.

--- inc/extras.php
+++ inc/extras.php
@@ -1,13 +1,31 @@
 <?php
+/*
+ * CUSTOM SETTINGS TABLE ROWS WITH VARIABLES
+ * Generates table rows template for use in the Dashboard.
+ *
+ * @since 0.1
+ */
+function rwp_usamls_admin_tr( $usamls_property_type, $usamls_property_ID, $usamls_wp_setting, $usamls_wp_shortcode ) {
+	echo '<tr><th><h4>Property Type ' . $usamls_property_ID . '<br />(' . $usamls_property_type . ')</h4></th><td>';
+	echo '<input type="text" id="rwp_id' . $usamls_property_ID . '_1" name="' . $usamls_wp_setting . '" value="' . get_option( $usamls_wp_setting ) . '" class="regular-text" />';
+	if(  get_option( 'rwp_usamls_net_url' ) && !get_option( $usamls_wp_setting )  ) {
+		echo '<p>Copy and paste the following URL ';
+		echo '<input type="text" id="rwp_id' . $usamls_property_ID . '_2" value="' . get_option( 'rwp_usamls_net_url' ) . '/default.asp?content=results&this_format=' . $usamls_property_ID . '" class="regular-text" /> or ';
+		echo '<a href="#" onClick="javascript:document.getElementById(\'rwp_id' . $usamls_property_ID . '_1\').value=document.getElementById(\'rwp_id' . $usamls_property_ID . '_2\').value" >click here</a> to fill it in, then click "Save".</p>';
+	}
+	if( get_option( $usamls_wp_setting ) ) {
+		echo '<p>' . $usamls_property_type . ' Shortcode: <code>[' . $usamls_wp_shortcode . ']</code></p>';
+	}
+	echo '</td></tr>' . "\n";
+}
+
 /**
  *  INTEGRATION CODE
- *  
+ *
  *	Adds the Search code to the textarea search widget code.
  *	All this crap needs to get cleaned up and turned into a f*ckin' widget.
  *	Yea.
  *
  *	@since 1.0
  */
-function rgvwp_usamls_net_textarea_widget_code() { ?>
-<form action="<?php echo get_option( 'rwp_usamls_net_shell_search' ); ?>" method="post"> <input type="hidden" name="do_search_results" value="true" /> <strong>MLS Number</strong> <table><tbody><tr><td><input type="text" name="item1" placeholder="MLS Number" /></td></tr></tbody></table> <strong>Area</strong> <table><tbody><tr><td><input type="text" name="item90" placeholder="City Name" /></td></tr></tbody></table> <strong>Price From</strong> <table><tbody><tr><td><input type="text" name="item2" placeholder="100" /></td><td><input type="text" name="item2_comma" placeholder="000" /></td></tr></tbody></table> <strong>Price To</strong> <table><tbody><tr><td><input type="text" name="item2a" placeholder="250" /></td><td><input type="text" name="item2a_comma" placeholder="000" /></td></tr></tbody></table> <input type="submit" value="Search MLS" /> </form>
-<?php }
+function rgvwp_usamls_net_textarea_widget_code() { ?><form action="<?php echo get_option( 'rwp_usamls_net_shell_search' ); ?>" method="post"> <input type="hidden" name="do_search_results" value="true" /> <strong>MLS Number</strong> <table><tbody><tr><td><input type="text" name="item1" placeholder="MLS Number" /></td></tr></tbody></table> <strong>Area</strong> <table><tbody><tr><td><input type="text" name="item90" placeholder="City Name" /></td></tr></tbody></table> <strong>Price From</strong> <table><tbody><tr><td><input type="text" name="item2" placeholder="100" /></td><td><input type="text" name="item2_comma" placeholder="000" /></td></tr></tbody></table> <strong>Price To</strong> <table><tbody><tr><td><input type="text" name="item2a" placeholder="250" /></td><td><input type="text" name="item2a_comma" placeholder="000" /></td></tr></tbody></table> <input type="submit" value="Search MLS" /></form><?php }
--- rwp_usamls_admin.php
+++ rwp_usamls_admin.php
@@ -22,15 +22,22 @@ include( plugin_dir_path( __FILE__ ) . '/inc/extras.php');
 	}
 	?>
 	<form method="post" action="options.php">
-	<?php settings_fields( 'rwp_usamls_sg_1' ); ?>
-	<?php do_settings_sections( 'rwp_usamls_sg_1' ); ?>
-	<h3>USAMLS.NET Settings</h3>
+	<?php
+	/*
+	 * NEED DOCUMENTATION
+ 	 *
+ 	 * @since 1.0
+	 */
+	settings_fields( 'rwp_usamls_sg_1' );
+	do_settings_sections( 'rwp_usamls_sg_1' );
+	?>
+	<h3><?php _e( 'USAMLS.NET Settings' ); ?></h3>
 	<table class="form-table">
 		<tbody>
 			<tr>
-				<th><?php _e( '<h4>USAMLS Username URL</h4>' ); ?></th>
+				<th><h4><?php _e( 'USAMLS Username URL' ); ?></h4></th>
 				<td>
-					<input type="text" name="rwp_usamls_net_url" value="<?php echo get_option( 'rwp_usamls_net_url' ); ?>" class="regular-text ltr" />
+					<input type="text" name="rwp_usamls_net_url" value="<?php echo get_option( 'rwp_usamls_net_url' ); ?>" class="regular-text" />
 					<?php
 						if( get_option( 'rwp_usamls_net_url' ) ) {
 							echo '<code>' . get_option( 'rwp_usamls_net_url' ) . '</code>';
@@ -41,33 +48,22 @@ include( plugin_dir_path( __FILE__ ) . '/inc/extras.php');
 					?>
 				</td>
 			</tr>
-	<?php
-	/*
-	 * CUSTOM SETTINGS TABLE ROWS WITH VARIABLES
-	 * Generates template for use in the Dashboard.
-	 *
-	 * @ since 0.1
-	 */
-			function rwp_usamls_admin_tr( $usamls_property_type, $usamls_property_ID, $usamls_wp_setting, $usamls_wp_shortcode ) {
-				echo '				<tr><th><h4>Property Type ' . $usamls_property_ID . '<br />(' . $usamls_property_type . ')</h4></th><td>';
-				echo '<input type="text" id="rwp_id' . $usamls_property_ID . '_1" name="' . $usamls_wp_setting . '" value="' . get_option( $usamls_wp_setting ) . '" class="regular-text ltr" />';
-				if(  get_option( 'rwp_usamls_net_url' ) && !get_option( $usamls_wp_setting )  ) {
-					echo '<p>Copy and paste the following URL ';
-					echo '<input type="text" id="rwp_id' . $usamls_property_ID . '_2" value="' . get_option( 'rwp_usamls_net_url' ) . '/default.asp?content=results&this_format=' . $usamls_property_ID . '" class="regular-text ltr" /> or ';
-					echo '<a href="#" onClick="javascript:document.getElementById(\'rwp_id' . $usamls_property_ID . '_1\').value=document.getElementById(\'rwp_id' . $usamls_property_ID . '_2\').value" >click here</a> to fill it in, then click "Save".</p>';
-				}
-				if( get_option( $usamls_wp_setting ) ) {
-					echo '<p>' . $usamls_property_type . ' Shortcode: <code>[' . $usamls_wp_shortcode . ']</code></p>';
-				}
-				echo '</td></tr>' . "\n";
-			}
+			<?php
+			/*
+			 * CUSTOM SETTINGS TABLE ROWS WITH VARIABLES
+			 * Generates template for use in the Dashboard. The main function is found in
+			 * inc/extras.php.
+			 *
+			 * @function rwp_usamls_admin_tr();
+			 * @since 0.1
+			 */
 			rwp_usamls_admin_tr( 'Residential', 1, 'rwp_usamls_net_search', 'usamls' );
 			rwp_usamls_admin_tr( 'Multi-Unit', 2, 'rwp_usamls_net_search_mu', 'usamls_multi' );
 			rwp_usamls_admin_tr( 'Land', 3, 'rwp_usamls_net_search_land', 'usamls_land' );
 			rwp_usamls_admin_tr( 'Commercial', 4, 'rwp_usamls_net_search_commercial', 'usamls_com' );
 			rwp_usamls_admin_tr( 'Rental', 5, 'rwp_usamls_net_search_rental', 'usamls_rental' );
 			rwp_usamls_admin_tr( 'Commercial Lease', 6, 'rwp_usamls_net_search_com_lease', 'usamls_com_lease' );
-	?>
+			?>
 		</tbody>
 	</table>
 
@@ -80,7 +76,7 @@ include( plugin_dir_path( __FILE__ ) . '/inc/extras.php');
 					<?php _e( '<p>This is your Residential Search page for Widget use.</p>' ); ?>
 				</th>
 				<td>
-					<input type="text" name="rwp_usamls_net_shell_search" value="<?php echo get_option( 'rwp_usamls_net_shell_search' ); ?>" />
+					<input type="text" name="rwp_usamls_net_shell_search" value="<?php echo get_option( 'rwp_usamls_net_shell_search' ); ?>" class="regular-text" />
 					<?php if ( get_option( 'rwp_usamls_net_shell_search' ) ) { ?>
 						<?php _e( '<p>Shortcode: <code>[usamls_search]</code></p> ' ); ?>
 						<div>
